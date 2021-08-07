217d99eb008908da492cfa5e2cdeb0fa77d31b0a
Gabriela Paredes <gabiprds.93@gmail.com>
2021-08-06 22:47:38 -0500

    Add variables for styles. Add more information about commit. Add styles

--- src/components/home/Commits/Commits.styles.tsx
+++ src/components/home/Commits/Commits.styles.tsx
@@ -5,9 +5,44 @@ import { CommitsStyledProps as Props } from "./Commits.types";
 
 const CommitsStyled = styled.div<Props>`
   &.Commits {
+    padding: 3.6rem;
   }
 
   .Commits {
+    &__title {
+      padding-bottom: 2.4rem;
+    }
+
+    &__item {
+      align-items: flex-start;
+      border: 1px solid var(--palette-border);
+      display: flex;
+      flex-direction: column;
+      padding: 0.8rem 1.6rem;
+
+      &:hover {
+        background-color: var(--palette-background);
+      }
+
+      &__message {
+        font-size: 1.4rem;
+        margin-bottom: 0.4rem;
+      }
+
+      &__user {
+        font-size: 1.2rem;
+      }
+
+      & a {
+        color: var(--palette-text);
+        font-weight: 600;
+        text-decoration: none;
+
+        &:hover {
+          text-decoration: underline;
+        }
+      }
+    }
   }
 `;
 
--- src/components/home/Commits/Commits.tsx
+++ src/components/home/Commits/Commits.tsx
@@ -11,15 +11,36 @@ const Commits: React.FC<Props> = (props) => {
 
   return (
     <Styles className="Commits">
-      <h1>Commits</h1>
+      <h1 className="Commits__title">Commits</h1>
 
       {isLoading ? <p>Loading...</p> : null}
 
       <ul>
         {commits?.map((commitInfo) => {
-          const { commit } = commitInfo;
+          const { commit, committer, html_url } = commitInfo;
+          const { login, html_url: htmlCommiter } = committer;
 
-          return <li>{commit.message}</li>;
+          return (
+            <li className="Commits__item">
+              <a
+                href={html_url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="Commits__item__message"
+              >
+                {commit.message}
+              </a>
+
+              <a
+                href={htmlCommiter}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="Commits__item__user"
+              >
+                {login}
+              </a>
+            </li>
+          );
         })}
       </ul>
     </Styles>
